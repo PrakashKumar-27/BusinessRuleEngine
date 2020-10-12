62b7cacb51aeb699201ed6d443b99ec03e812527
pkumar25 <prakash.kumar25@mrcooper.com>
2020-10-12 11:05:03 +0530

    Adding Failure Test Case for Product Payment

--- BusinessRuleEngine/BusinessRuleEngineTest/BusinessRuleEngineTest.cs
+++ BusinessRuleEngine/BusinessRuleEngineTest/BusinessRuleEngineTest.cs
@@ -10,11 +10,22 @@ namespace BusinessRuleEngineTest
         [TestMethod]
         public void BookPaymentTest()
         {
-            PackingSlip packingSlip = new PackingSlip() { CustomerId = 1, PaymentSlipId = 1, Amount = 10000, AgentCommisionAmount=200, EmailSentFor= "Activation Membership Email", FreeAidVideo=false, paymentFor = PaymentFor.Shipping };
+            PackingSlip packingSlip = new PackingSlip() { CustomerId = 1, PaymentSlipId = 1, Amount = 10000, AgentCommisionAmount=200, paymentFor = PaymentFor.Shipping };
             CustomerPayment customerPayment = new CustomerPayment() { CustomerId = 1 , Amount = 10000 };
             Payment bookPayment = new BookPayment(customerPayment);
             var result =  bookPayment.ProcessPayment();
-            Assert.AreEqual(packingSlip.paymentFor, PaymentFor.Shipping);
+            Assert.AreEqual(packingSlip.paymentFor, result.paymentFor);
+            Assert.AreEqual(packingSlip.AgentCommisionAmount, result.AgentCommisionAmount);
+        }
+
+        [TestMethod]
+        public void ProductPaymentTest()
+        {
+            PackingSlip packingSlip = new PackingSlip() { CustomerId = 1, PaymentSlipId = 1, Amount = 10000, AgentCommisionAmount = 200, EmailSentFor = "Activation Membership Email", FreeAidVideo = false, paymentFor = PaymentFor.RoyaltyDepartment };
+            CustomerPayment customerPayment = new CustomerPayment() { CustomerId = 1, Amount = 10000 };
+            Payment bookPayment = new BookPayment(customerPayment);
+            var result = bookPayment.ProcessPayment();
+            Assert.AreEqual(packingSlip.paymentFor, result.paymentFor);
             Assert.AreEqual(packingSlip.AgentCommisionAmount, result.AgentCommisionAmount);
         }
     }
